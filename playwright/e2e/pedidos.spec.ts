c51daa10d9f00df2caf30d0e43f8c0ca993bbd3d
Leonardo Freitas <leofrts.qa@gmail.com>
2026-08-08 13:20:57 -0300

    Padronização de Projeto - AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,22 +1,22 @@
 import { test, expect } from '@playwright/test'
 
-test('Deve consultar um pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/')
-  
-  // Checkpoint!
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-  
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-  // Checkpoint!
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
-  
-  await page.getByTestId('search-order-id').fill('VLO-Q2W1PO')
+// AAA - Arrange, Act, Assert
 
+test('Deve consultar um pedido aprovado', async ({ page }) => {
+  // Arrange
+  await page.goto('http://localhost:5173/')
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+ 
+  // Act
+  await page.getByTestId('search-order-id').fill('VLO-Q2W1PO')
   await page.getByTestId('search-order-button').click()
  
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-Q2W1PO')
-  
+
   await expect(page.getByTestId('order-result-status')).toBeVisible()
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
 })
